40c041697d273b3aafa2dc36c077ddb78416ee31
Shailesh <shailesh.kumar@outlook.com>
2020-05-30 23:04:51 +0200

    print out pull request payload

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ try {
   core.setOutput("time", time);
 
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context, undefined, 2)
+  const payload = JSON.stringify(github.context.payload.pull_request, undefined, 2)
   console.log(`The event payload: ${payload}`);
 
 } catch (error) {
