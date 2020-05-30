a30c22ff7a5042f75a763cb23a9a67b7e6fb477e
Shailesh <shailesh.kumar@outlook.com>
2020-05-30 23:20:47 +0200

    check title

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const github = require('@actions/github');
 
 try {
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload.pull_request, undefined, 2)
+  const payload = JSON.stringify(github.context.payload.pull_request.title, undefined, 2)
   console.log(`The event payload: ${payload}`);
 
 } catch (error) {
