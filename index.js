11df20d4995cea34ad2c084d53f38d33094acf95
Shailesh <shailesh.kumar@outlook.com>
2020-05-30 23:26:56 +0200

    print body

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ try {
   const title = JSON.stringify(github.context.payload.pull_request.title, undefined, 2)
   const body  = JSON.stringify(github.context.payload.pull_request.body, undefined, 2)
   console.log(`pr title: ${title}`);
-  console.log(`pt body: ${body}`);
+  console.log(`pr body: ${body}`);
 
 } catch (error) {
   core.setFailed(error.message);
