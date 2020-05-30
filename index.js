203447af7ed1c54d03f974108cedf15923e26292
Shailesh <shailesh.kumar@outlook.com>
2020-05-30 23:18:11 +0200

    bunch of stuff

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  // set an output variable time with value time of greeting
-  core.setOutput("time", time);
-
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload.pull_request, undefined, 2)
   console.log(`The event payload: ${payload}`);
