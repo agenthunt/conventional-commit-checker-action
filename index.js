bcf9feeab7da754f84b505310b4bb51d0a422cdf
Shailesh <shailesh.kumar@outlook.com>
2020-06-03 00:29:01 +0200

    testing regex

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ try {
   const body  = JSON.stringify(github.context.payload.pull_request.body, undefined, 2)
   console.log(`pr title: ${title}`);
   console.log(`pr body: ${body}`);
+  const prTitleRegexPattern = core.getInput("pr-title-regex");
+  
+  const prTitleRegExp = new RegExp(prTitleRegexPattern);
+  if(!prTitleRegExp.test(title)){
+    core.setFailed(`Pull request title does not match ${prTitleRegexPattern}`);
+    return;
+  }
+
+  const prBodyRegexPattern = core.getInput("pr-body-regex");
+  const prBodyRegExp = new RegExp(prBodyRegexPattern);
+  if(!prBodyRegExp.test(body)){
+    core.setFailed(`Pull request body does not match ${prBodyRegexPattern}`);
+    return;
+  }
 
 } catch (error) {
   core.setFailed(error.message);
