afdbd641f29ef3497bdad552091aa7d8845d7ac0
Shailesh <shailesh.kumar@outlook.com>
2020-06-03 00:56:59 +0200

    dsdf

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ function escapeRegExp(string) {
 }
 
 try {
-  const title = JSON.stringify(github.context.payload.pull_request.title, undefined, 2)
-  const body  = JSON.stringify(github.context.payload.pull_request.body, undefined, 2)
+  const title = github.context.payload.pull_request.title;
+  const body  = github.context.payload.pull_request.body;
 
   const prTitleRegexPattern = core.getInput("pr-title-regex");
   
-  const prTitleRegExp = new RegExp(escapeRegExp(prTitleRegexPattern));
+  const prTitleRegExp = new RegExp(prTitleRegexPattern);
   if(!prTitleRegExp.test(title)){
     core.setFailed(`Pull request title does not match ${prTitleRegexPattern}`);
     return;
   }
 
   const prBodyRegexPattern = core.getInput("pr-body-regex");
-  const prBodyRegExp = new RegExp(escapeRegExp(prBodyRegexPattern));
+  const prBodyRegExp = new RegExp(prBodyRegexPattern);
   if(!prBodyRegExp.test(body)){
     core.setFailed(`Pull request body does not match ${prBodyRegexPattern}`);
     return;
